e8162c67edacc4b02871c18d0bbf0108296bf0fd
nopalll <tssytari@gmail.com>
2026-06-05 08:14:04 +0700

    done:interaksi user

--- be/app/Models/Follow.php
+++ be/app/Models/Follow.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Follow extends Model
+class Follow extends Pivot
 {
     use HasFactory, HasUuids;
 
