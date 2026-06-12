7de4cae4700b04917a25f741c5a10e1170f18119
Kazeku-06 <tssytari@gmail.com>
2026-06-12 15:09:07 +0700

    benerin logic be sama fe

--- be/database/seeders/BadgeSeeder.php
+++ be/database/seeders/BadgeSeeder.php
@@ -11,11 +11,11 @@ class BadgeSeeder extends Seeder
     public function run()
     {
         $badges = [
-            ['name' => 'Newbie', 'required_points' => 0, 'achievement_type' => 'points', 'threshold' => 0, 'description' => 'Selamat datang!'],
-            ['name' => 'Rising Star', 'required_points' => 100, 'achievement_type' => 'points', 'threshold' => 100, 'description' => '100 poin reputasi'],
-            ['name' => 'Talkative', 'achievement_type' => 'comment_count', 'threshold' => 10, 'description' => '10 komentar'],
-            ['name' => 'Problem Solver', 'achievement_type' => 'accepted_count', 'threshold' => 1, 'description' => 'Jawaban pertama diterima'],
-            ['name' => 'Prolific', 'achievement_type' => 'post_count', 'threshold' => 5, 'description' => '5 postingan'],
+            ['name' => 'Newbie', 'required_points' => 0, 'achievement_type' => 'points', 'threshold' => 0, 'description' => 'Selamat datang!', 'icon' => '🌱'],
+            ['name' => 'Rising Star', 'required_points' => 100, 'achievement_type' => 'points', 'threshold' => 100, 'description' => '100 poin reputasi', 'icon' => '⭐'],
+            ['name' => 'Talkative', 'achievement_type' => 'comment_count', 'threshold' => 10, 'description' => '10 komentar', 'icon' => '💬'],
+            ['name' => 'Problem Solver', 'achievement_type' => 'accepted_count', 'threshold' => 1, 'description' => 'Jawaban pertama diterima', 'icon' => '🏆'],
+            ['name' => 'Prolific', 'achievement_type' => 'post_count', 'threshold' => 5, 'description' => '5 postingan', 'icon' => '✍️'],
         ];
 
         foreach ($badges as $badge) {
@@ -23,7 +23,7 @@ class BadgeSeeder extends Seeder
                 'name' => $badge['name'],
                 'slug' => Str::slug($badge['name']),
                 'description' => $badge['description'],
-                'icon' => null,
+                'icon' => $badge['icon'],
                 'required_points' => $badge['required_points'] ?? null,
                 'achievement_type' => $badge['achievement_type'],
                 'threshold' => $badge['threshold'],
--- fe/src/features/questions/QuestionDetailView.tsx
+++ fe/src/features/questions/QuestionDetailView.tsx
@@ -9,7 +9,6 @@ import { timeAgo, formatCount, cn } from "@/lib/utils";
 import Spinner from "@/components/ui/Spinner";
 import Avatar from "@/components/ui/Avatar";
 import VoteButton from "@/components/post/VoteButton";
-import ReportButton from "@/components/post/ReportButton";
 import CommentSection from "./CommentSection";
 import ReportButton from "@/components/post/ReportButton";
 
